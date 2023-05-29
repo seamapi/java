6ea816f4ca3ed408b0f308425970e172b4464c4b
Deep Singhvi <deep@usebirch.com>
2023-05-29 09:40:24 -0400

    fix: add access codes update and wait (#1)
    * add access codes update and wait
    
    * fix fernignore paths

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -1,19 +1,31 @@
 package sample;
 
 import com.seam.api.resources.accesscodes.requests.AccessCodesCreateRequest;
+import com.seam.api.resources.accesscodes.requests.AccessCodesUpdatePutRequest;
 import com.seam.api.types.AccessCodesCreateResponse;
 import java.lang.String;
 import com.seam.api.SeamApiClient;
 
 public final class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     SeamApiClient seam = SeamApiClient.builder()
             .token("MY_API_KEY")
             .build();
+
+    // create access code
     AccessCodesCreateResponse accessCodesCreateResponse = seam.accessCodes().create(AccessCodesCreateRequest.builder()
             .deviceId("123e4567-e89b-12d3-a456-426614174000")
             .commonCodeKey("My first code")
             .build());
     System.out.println(accessCodesCreateResponse.getAccessCode());
+
+    Object accessCode = seam.accessCodes().updateAndWaitUntilReady(AccessCodesUpdatePutRequest.builder()
+            .accessCodeId("my-access-code-id")
+            .name("access-code-name")
+            .code("access-code")
+            .build());
+    System.out.println(accessCode);
   }
+
+
 }
