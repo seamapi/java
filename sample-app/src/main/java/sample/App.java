b2eef39d7df7a29ad3e24b2ff25d391503663e42
dsinghvi <deep@usebirch.com>
2023-08-07 11:39:24 -0400

    list devices works

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -1,9 +1,16 @@
 package sample;
 
+import com.seam.api.resources.devices.requests.DevicesListRequest;
 import java.lang.String;
+import com.seam.api.SeamApiClient;
 
 public final class App {
   public static void main(String[] args) {
-    // import com.seam.api.SeamApiClient
+    SeamApiClient seam = SeamApiClient.builder()
+            .apiKey("YOUR_TOKEN")
+            .build();
+
+    var response = seam.devices().list(DevicesListRequest.builder().build());
+    System.out.println(response);
   }
 }
