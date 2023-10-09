6fc45b84cdec82a2472a669dace9dfe13e7b01b2
dsinghvi <deep@buildwithfern.com>
2023-10-09 15:19:57 -0400

    add thermostat tests

--- src/test/java/com/seam/api/noiseThresholds/NoiseThresholdsTest.java
+++ src/test/java/com/seam/api/noiseThresholds/NoiseThresholdsTest.java
@@ -2,22 +2,16 @@ package com.seam.api.noiseThresholds;
 
 import com.seam.api.Seam;
 import com.seam.api.TestUtils;
-import com.seam.api.resources.devices.requests.DevicesListRequest;
-import com.seam.api.resources.events.requests.EventsGetRequest;
-import com.seam.api.resources.events.requests.EventsListRequest;
 import com.seam.api.resources.noisesensors.noisethresholds.requests.NoiseThresholdsCreateRequest;
 import com.seam.api.resources.noisesensors.noisethresholds.requests.NoiseThresholdsDeleteRequest;
 import com.seam.api.resources.noisesensors.noisethresholds.requests.NoiseThresholdsListRequest;
 import com.seam.api.types.Device;
-import com.seam.api.types.Event;
 import com.seam.api.types.NoiseThreshold;
-import com.seam.api.types.NoiseThresholdsCreateResponse;
 import java.util.List;
-import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
-public class NoiseThresholdsTest {
+public final class NoiseThresholdsTest {
 
     private static Seam seam;
 
@@ -46,14 +40,14 @@ public class NoiseThresholdsTest {
         noiseThresholds = getMinutDeviceNoiseThresholds(device.getDeviceId());
         Assertions.assertThat(noiseThresholds).hasSize(1);
 
-        NoiseThresholdsCreateResponse response = seam.noiseSensors().noiseThresholds().create(NoiseThresholdsCreateRequest.builder()
+        seam.noiseSensors().noiseThresholds().create(NoiseThresholdsCreateRequest.builder()
                 .deviceId(device.getDeviceId())
                 .startsDailyAt("20:00:00[America/Los_Angeles]")
                 .endsDailyAt("08:00:00[America/Los_Angeles]")
                 .noiseThresholdDecibels(75.0)
                 .build());
-
-        response.getActionAttempt()
+        noiseThresholds = getMinutDeviceNoiseThresholds(device.getDeviceId());
+        Assertions.assertThat(noiseThresholds).hasSize(2);
     }
 
     public List<NoiseThreshold> getMinutDeviceNoiseThresholds(String deviceId) {
